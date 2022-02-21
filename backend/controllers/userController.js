59b7fc879a88a6dd3fce77e9f5b4f8832721333d
Elias <elias.roodrigues98@gmail.com>
2022-02-21 18:06:55 -0300

    added frontend UI

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,7 +19,7 @@ const registerUser = asyncHandler(async(req, res) => {
 
   if (userExists) {
     res.status(400);
-    throw new Error('User alewady exists');
+    throw new Error('User already exists');
   }
 
   // hash password
